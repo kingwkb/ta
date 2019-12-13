78c0fee29c5bdab8d81da455d943ae05296cd3b6
kingwkb <kingwkb@gmail.com>
2019-12-13 20:15:15 +0800

    init

---
+++ precache-manifest.a338cc8f99c33d21051673546391ae14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbf40bd6ae1493f457e84c3ddd5dd2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5231b07f94851c1affdc",
+    "url": "./static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "52324d0e4e095d82eb43",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5231b07f94851c1affdc",
+    "url": "./static/js/2.3ee9d4e9.chunk.js"
+  },
+  {
+    "revision": "52324d0e4e095d82eb43",
+    "url": "./static/js/main.73b2be72.chunk.js"
+  },
+  {
+    "revision": "e7e7d897dbd6c57f9040",
+    "url": "./static/js/runtime-main.44d4c2bb.js"
+  }
+]);
